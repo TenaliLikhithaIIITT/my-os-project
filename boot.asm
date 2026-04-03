9436ca72412b53e43029608ab6035763b3d9f0db
eepsi230307 <eepsi230307@gmail.com>
2026-04-03 21:55:02 +0530

    Final output HELLO WORLD

--- boot.asm
+++ boot.asm
@@ -2,11 +2,20 @@
 [ORG 0x7c00]
 
 start:
-    mov ah, 0x0e
-    mov al, 'H'
-    int 0x10
+    mov si, message
 
+print:
+    lodsb
+    cmp al, 0
+    je done
+    mov ah, 0x0e
+    int 0x10
+    jmp print
+
+done:
     jmp $
+
+message db 'HELLO WORLD', 0
 
 times 510-($-$$) db 0
 dw 0xaa55
--- kernel.c
+++ kernel.c
@@ -1,9 +1,11 @@
  void kernel_main() {
     char *video_memory = (char*) 0xb8000;
 
-    video_memory[0] = 'H';
-    video_memory[2] = 'E';
-    video_memory[4] = 'L';
-    video_memory[6] = 'L';
-    video_memory[8] = 'O';
+    char *msg = "HELLO WORLD";
+
+    for(int i = 0; msg[i] != '\0'; i++) {
+	video_memory[i * 2] = msg[i];
+	video_memory[i * 2 +1] = 0x07;
+    }
+
 }
