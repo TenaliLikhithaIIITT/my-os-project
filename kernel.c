b7bb2ed19da2359eccf3645cf47c890cd7c58056
Tenali Likhitha <tenalilikhitha7@gmail.com>
2026-04-03 21:24:46 +0530

    Update kernel.c

--- kernel.c
+++ kernel.c
@@ -1,4 +1,9 @@
-void kernel_main() {
+ void kernel_main() {
     char *video_memory = (char*) 0xb8000;
+
     video_memory[0] = 'H';
+    video_memory[2] = 'E';
+    video_memory[4] = 'L';
+    video_memory[6] = 'L';
+    video_memory[8] = 'O';
 }
